0f7d54686a450903f336e66857a0a0fa5b348455
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-02-05 23:04:43 +0200

    lap 06

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
  var books = 0;
+ var visa;
+ var name;
  function takeUserInfo(){
-    var name = prompt('what\'s your name')
-    var age = prompt('what\'s your age')
-    if(age >= 18){
-        alert(`welcom ${name} we will show you books good for your age ${age}`)
-    }else{
-        alert('you are not allowed to access the bookstory sorry') 
-        window.history.back();
-    } 
-    
+     name = prompt('what\'s your name')
+     visa = prompt('what\'s your cridit number')    
  }
 
 takeUserInfo()
-// window.history.go()
 
 function click(event){
     books++
-    console.log(books)
+}
+
+function close(){
+    open(location, '_self').close();
+}
+
+function confirmpay(){
+   if(confirm(`you choose ${books} do you want to continuo`)){
+    var count = 3
+    var confirme = prompt('write your cridit card to confirme buyment')
+    while(visa != confirme){
+        console.log(count,count===3)
+        if(count === 0){
+            prompt('sorry we can\'t sell you have a good day')
+            return close()
+             break;
+            // count++
+            // console.log(count)
+
+        }
+        confirme = prompt(`you have ${count} chance`)
+        count--
+    }
+    alert('we will send your books')
+   }{
+
+   }
 }
 
