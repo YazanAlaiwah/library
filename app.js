40dc9a8fbc0e50165b06a8e85fe7ef6e03a13ff5
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-02-06 09:29:55 +0200

    finsh

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
  var books = 0;
  var visa;
  var name;
+ var addImages='';
  function takeUserInfo(){
      name = prompt('what\'s your name')
      visa = prompt('what\'s your cridit number')    
  }
+ var imgs = ['https://i.ytimg.com/vi/848q7WLmwbk/maxresdefault.jpg','https://i.ytimg.com/vi/VlsQC87i06w/maxresdefault.jpg','https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/War_official_poster.jpg/220px-War_official_poster.jpg','https://www.foxmovies.com/s3/dev-temp/en-US/__5d92439d566e1.jpg','https://knnit.com/wp-content/uploads/2019/12/Movie-Genres-Types-of-Movies-List-of-Genres-and-Categories-Header-StudioBinder.jpg']
+
+ function addBooks() {
+    var howManyBooks = Number(prompt('how many books you wnat'))
+    for(let i = 0; i < howManyBooks;i++){
+        addImages+= `<img src="${imgs[i%imgs.length]}">`
+    }
+    var target = document.querySelector('main');
+    var div = document.createElement('div');
+    div.innerHTML = addImages;
+    target.parentNode.insertBefore( div, target.nextSibling );
+
+ }
+
 
 takeUserInfo()
 
